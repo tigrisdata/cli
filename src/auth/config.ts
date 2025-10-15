db856b6aecf0c023c4bf7a60dc889f9f25404681
A Ibrahim <hello@designcode.me>
2025-10-15 13:19:31 +0200

    feat: implement list bucket

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -13,9 +13,9 @@ export interface Auth0Config {
  */
 export function getAuth0Config(): Auth0Config {
   return {
-    domain: process.env.AUTH0_DOMAIN || 'tigrisdata-dev.us.auth0.com',
+    domain: process.env.AUTH0_DOMAIN || 'auth-dev.tigris.dev',
     clientId: process.env.AUTH0_CLIENT_ID || 'JdJVYIyw0O1uHi5L5OJH903qaWBgd3gF',
-    audience: process.env.AUTH0_AUDIENCE || 'https://tigris-cli-dev',
+    audience: process.env.AUTH0_AUDIENCE || 'https://tigris-api-dev',
   };
 }
 
